76cb4a3fc2145b5933d55248d79eb2ef365949fc
anbordios <annbordios15@gmail.com>
2024-09-22 21:59:11 +0800

    Add files via upload

---
+++ LA1_BORDIOSJA_CS2C/activity 2.js
@@ -0,0 +1,9 @@
+let firstFavNum = 3; // Declare the first favorite number
+let secondFavNum = 5; // Declare another second favorite number
+
+console.log(firstFavNum + secondFavNum); // Add the two values, it returns 8
+console.log(firstFavNum - secondFavNum); // Subtract the two values, it returns -2
+console.log(firstFavNum * secondFavNum); // Multiply the two values, it returns 15 
+console.log(firstFavNum / secondFavNum); // Divide the two values, it returns 0.6
+console.log(firstFavNum % secondFavNum); // In Modulus, the remainder is 3
+console.log(firstFavNum ** secondFavNum); // The power of the two values is equivalent to 243
