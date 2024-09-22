4fcb172a05854e36d8492cf0401dd815683a03d9
anbordios <annbordios15@gmail.com>
2024-09-23 00:26:55 +0800

    Add files via upload

---
+++ LA1_BORDIOSJA_CS2D/activity5.js
@@ -0,0 +1,13 @@
+let favActorFirstName = "Tom";
+let favActorLastName = "Cruise";
+let fullName = favActorFirstName+" "+favActorLastName; // Concatenate the favActorFirstName and favActorLastName to complete its fullname.
+let uppercase = favActorFirstName.toUpperCase(); // Change the favActorFirstName to uppercase i
+let message =  ' My favorite actor is '+uppercase+' because he is so cool!';
+
+message += 'His best show is Silicon Valley'; // Append the variable message to insert, "His best show is Silicon Valley"
+
+console.log(favActorFirstName);
+console.log(favActorLastName);
+console.log(fullName);
+console.log(uppercase);
+console.log(message);
