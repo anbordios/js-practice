074bbcf48b4c9d748e0435417c41af4c50185cf6
anbordios <annbordios15@gmail.com>
2024-09-22 21:49:43 +0800

    Upload inside the folder

--- LA1_BORDIOSJA_CS2C/activity1.js
+++ LA1_BORDIOSJA_CS2C/activity1.js
@@ -1,1 +1,9 @@
+let name = "Jhury-Ann";
+let whatDoYouWannaBecomeInYourLife = "programmer";
+let gender = "Female";
+let facebookAcct = "Jhury-Ann Valdez Bordios";
 
+console.log(name); // The output will returns all the values declared in the variables 
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(facebookAcct);
