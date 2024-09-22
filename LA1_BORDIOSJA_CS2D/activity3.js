4917b4613d252d6c5e369e117efc74e3db81fcf9
anbordios <annbordios15@gmail.com>
2024-09-22 23:34:15 +0800

    Add files via upload

---
+++ LA1_BORDIOSJA_CS2D/activity3.js
@@ -0,0 +1,9 @@
+let isJsProgrammingLanguage = true;
+let isJsHard = false;
+let favNumb = 5;
+let add = favNumb + undefined; //This results in NaN because undefined is not a number, so any arithmetic operation with undefined returns NaN.
+
+console.log(add)
+console.log(isJsProgrammingLanguage);
+console.log(isJsHard);
+console.log(favNumb);
