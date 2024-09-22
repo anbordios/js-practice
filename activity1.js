7394eda5d6c10e70392aad1b70f2ff7d254460f5
anbordios <annbordios15@gmail.com>
2024-09-22 20:39:50 +0800

    activity1.js

--- activity1.js
+++
@@ -1,9 +0,0 @@
-let name = "Jhury-Ann Bordios";
-let whatDoYouWannaBecomeInYourLife = "programmer";
-let gender = "Female";
-let facebookAcct = "Jhury-Ann Valdez Bordios";
-
-console.log(name);
-console.log(whatDoYouWannaBecomeInYourLife);
-console.log(gender);
-console.log(facebookAcct);
